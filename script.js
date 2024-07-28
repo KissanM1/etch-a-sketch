7c9fa5ed6f12a9639f41a346f20ebb6b69da84f9
Kissan <173943794+KissanM1@users.noreply.github.com>
2024-07-28 19:23:33 -0400

    Adds new grid button

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
+newGrid(16);
 
-for (let i = 0; i < 16; i++){
-    let row = document.createElement("div");
-    row.className = "row";
-        for (let j = 0; j < 16; j++) {
-            let div = document.createElement("div");
-            div.className = "grid-box"
-            div.addEventListener("mouseenter", function() {
-                console.log("Mouse entered div", i + 1, j + 1);
-                div.style.backgroundColor = "red";
-            });
-            row.appendChild(div);    
-        }
-    container.appendChild(row);
+const newGridButton = document.querySelector(".new-grid");
+newGridButton.addEventListener("click", reSizeGrid);
+
+function newGrid(length){
+    grid.innerHTML = "";
+    for (let i = 0; i < length; i++){
+        let row = document.createElement("div");
+        row.className = "row";
+            for (let j = 0; j < length; j++) {
+                let div = document.createElement("div");
+                div.className = "grid-box"
+                div.addEventListener("mouseenter", function() {
+                    console.log("Mouse entered div", i + 1, j + 1);
+                    div.style.backgroundColor = "red";
+                });
+                row.appendChild(div);    
+            }
+        grid.appendChild(row);
+    }
+}
+
+function reSizeGrid(){
+    length = prompt("How many squares would you like the girds length ot be");
+    newGrid(length);
 }
